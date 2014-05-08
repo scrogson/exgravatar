a72a5f7f221627198ec900fbf92ab7fb37de1385
Igor Kapkov <igasgeek@me.com>
2014-05-09 02:49:20 +0400

    change maps to dict

--- test/exgravatar_test.exs
+++ test/exgravatar_test.exs
@@ -10,16 +10,16 @@ defmodule ExgravatarTest do
   end
 
   test "it generates secure urls" do
-    assert "https://secure.#{@domain}/#{@email_hash}" == Exgravatar.generate(@email, %{}, true)
+    assert "https://secure.#{@domain}/#{@email_hash}" == Exgravatar.generate(@email, [], true)
   end
 
   test "it generates correct query string params" do
-    options = %{s: 256, r: "pg"}
-    assert "https://secure.#{@domain}/#{@email_hash}?r=pg&s=256" == Exgravatar.generate(@email, options, true)
+    options = [s: 256, r: "pg"]
+    assert "https://secure.#{@domain}/#{@email_hash}?s=256&r=pg" == Exgravatar.generate(@email, options, true)
   end
 
   test "it allows you to use a default image" do
-    options = %{d: "http://example.com/images/avatar.jpg"}
+    options = [d: "http://example.com/images/avatar.jpg"]
     assert "http://#{@domain}/#{@email_hash}?d=http%3A%2F%2Fexample.com%2Fimages%2Favatar.jpg" == Exgravatar.generate(@email, options)
   end
 end
