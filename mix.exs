39314f0bea277cc6e860c3f90533db45ac88a8a7
Sonny Scroggin <scrogson@gmail.com>
2014-05-08 12:12:13 -0500

    Publish to Hex

--- mix.exs
+++ mix.exs
@@ -1,24 +1,37 @@
+Code.ensure_loaded?(Hex) and Hex.start
+
 defmodule Exgravatar.Mixfile do
   use Mix.Project
 
   def project do
     [ app: :exgravatar,
-      version: "0.0.1",
+      version: "0.1.0",
       elixir: "~> 0.13.1",
+      description: description,
+      package: package,
       deps: deps ]
   end
 
-  # Configuration for the OTP application
   def application do
     [applications: [:crypto]]
   end
 
-  # Returns the list of dependencies in the format:
-  # { :foobar, git: "https://github.com/elixir-lang/foobar.git", tag: "0.1" }
-  #
-  # To specify particular versions, regardless of the tag, do:
-  # { :barbat, "~> 0.1", github: "elixir-lang/barbat" }
   defp deps do
     []
+  end
+
+  defp description do
+    """
+    An Elixir module for generating Gravatar urls.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE"],
+      contributors: ["Sonny Scroggin"],
+      licenses: ["MIT"],
+      links: [ { "GitHub", "https://github.com/scrogson/exgravatar" } ]
+    ]
   end
 end
