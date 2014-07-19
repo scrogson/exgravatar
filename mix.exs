f71b6c16a9b16fea3a2e6496fbc5ebbf45c39d30
Sonny Scroggin <scrogson@gmail.com>
2014-07-18 19:38:02 -0500

    Bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Exgravatar.Mixfile do
 
   def project do
     [ app: :exgravatar,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: ">= 0.14.1",
       description: description,
       package: package,
