d5dc7dce2a8ae91d2e95ca45527e7ff891382c0e
Sonny Scroggin <sonny@scrogg.in>
2020-02-06 18:03:06 -0600

    mix format

--- mix.exs
+++ mix.exs
@@ -2,12 +2,14 @@ defmodule Exgravatar.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :exgravatar,
-     version: "2.0.1",
-     elixir: "~> 1.0",
-     description: description(),
-     package: package(),
-     deps: []]
+    [
+      app: :exgravatar,
+      version: "2.0.2",
+      elixir: "~> 1.0",
+      description: description(),
+      package: package(),
+      deps: []
+    ]
   end
 
   def application do
@@ -21,9 +23,11 @@ defmodule Exgravatar.Mixfile do
   end
 
   defp package do
-    [files: ["lib", "mix.exs", "README*", "LICENSE"],
-     maintainers: ["Sonny Scroggin"],
-     licenses: ["MIT"],
-     links: %{"GitHub" => "https://github.com/scrogson/exgravatar"}]
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE"],
+      maintainers: ["Sonny Scroggin"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/scrogson/exgravatar"}
+    ]
   end
 end
--- test/exgravatar_test.exs
+++ test/exgravatar_test.exs
@@ -12,21 +12,21 @@ defmodule ExgravatarTest do
 
   test "it converts email into an MD5 hash" do
     assert "#{@domain}/#{@email_hash}" ==
-      gravatar_url(@email, secure: false)
+             gravatar_url(@email, secure: false)
   end
 
   test "it generates secure urls by default" do
     assert "#{@secure}/#{@email_hash}" ==
-      gravatar_url(@email)
+             gravatar_url(@email)
   end
 
   test "it generates correct query string params" do
     assert "#{@secure}/#{@email_hash}?s=256&r=pg" ==
-      gravatar_url(@email, s: 256, r: "pg")
+             gravatar_url(@email, s: 256, r: "pg")
   end
 
   test "it allows you to use a default image" do
     assert "#{@secure}/#{@email_hash}?d=http%3A%2F%2Fexample.com%2Fimages%2Favatar.jpg" ==
-      gravatar_url(@email, d: "http://example.com/images/avatar.jpg")
+             gravatar_url(@email, d: "http://example.com/images/avatar.jpg")
   end
 end
